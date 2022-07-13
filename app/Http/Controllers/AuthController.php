e26507992fe7bedd53a7e3b5681d0d1c2070ce2f
angga <angga.haristia@bangsawancyberindo.co.id>
2022-07-13 10:59:32 +0700

    Penambahan Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,7 +24,6 @@ class AuthController extends Controller
             // Request to API 
             $requestAPI = Http::withHeaders([
                 'Content-type' =>  'application/json',
-                'Authorization' => ' Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTBiOWUxMzEtNjU3My00NzJlLTlhZjktNzU2NDNjYzNiYTQ0IiwicGFydG5lcl9pZCI6IjhhOWUwYWExLTE4YTMtNDIzYS1hN2M3LTQzY2JiOTk1ZjMxZCIsImVtYWlsIjoiYWRtaW5AcGVyc2FkaWEuY28uaWQiLCJpYXQiOjE2NTc1MTI0Mzh9.vRKKbO4Mm8DlY5NTcgXFTKBD5dt02onFvx8I9o4M0Ss',
             ])->post('http://103.214.53.148:51999/v1/login',[
                 'email' => $request->email,
                 'password' => $request->password
--- app/Http/Controllers/PromoController.php
+++ app/Http/Controllers/PromoController.php
@@ -11,7 +11,7 @@ class PromoController extends Controller
     public function index(){
         $request = Http::withHeaders([
             'Content-type' =>  'application/json',
-            'Authorization' => ' Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTBiOWUxMzEtNjU3My00NzJlLTlhZjktNzU2NDNjYzNiYTQ0IiwicGFydG5lcl9pZCI6IjhhOWUwYWExLTE4YTMtNDIzYS1hN2M3LTQzY2JiOTk1ZjMxZCIsImVtYWlsIjoiYWRtaW5AcGVyc2FkaWEuY28uaWQiLCJpYXQiOjE2NTc1MTI0Mzh9.vRKKbO4Mm8DlY5NTcgXFTKBD5dt02onFvx8I9o4M0Ss',
+            'Authorization' => ' Bearer '. session('token'),
         ])->get('http://103.214.53.148:51999/v1/promo');
         $response = $request->getBody()->getContents();
         $data = json_decode($response,true);
@@ -21,7 +21,7 @@ class PromoController extends Controller
     public function dtPromo(){
         $request = Http::withHeaders([
             'Content-type' =>  'application/json',
-            'Authorization' => ' Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTBiOWUxMzEtNjU3My00NzJlLTlhZjktNzU2NDNjYzNiYTQ0IiwicGFydG5lcl9pZCI6IjhhOWUwYWExLTE4YTMtNDIzYS1hN2M3LTQzY2JiOTk1ZjMxZCIsImVtYWlsIjoiYWRtaW5AcGVyc2FkaWEuY28uaWQiLCJpYXQiOjE2NTc1MTI0Mzh9.vRKKbO4Mm8DlY5NTcgXFTKBD5dt02onFvx8I9o4M0Ss',
+            'Authorization' => ' Bearer'.session('token'),
         ])->get('http://103.214.53.148:51999/v1/promo');
         $response = $request->getBody()->getContents();
         $json = json_decode($response,true);
